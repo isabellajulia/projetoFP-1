7370b26430e44013c6de1fd37201eb6493e20e1c
isabellajulia <bellajulia_ap@hotmail.com>
2014-05-15 00:58:53 -0300

    fluxo de caixa v1

---
+++ fluxo/urlsfluxo.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+urlpatterns = patterns('fluxo.views',
+	url(r'^$, 'fluxoListar'),
+	url(r'^pesquisar/$, 'fluxoPesquisar'),
+	)
---
+++ fluxo/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from datetime import datetime
+from caixas.models import Conta
+from pessoas.models import Pessoa
+
+def fluxoListar(request):
+	pessoas = Pessoa.objects.all().order_by('nome')
+	return render(request,'fluxo/fluxoListar.html', {'pessoas': pessoas})
+
+def fluxoPesquisar(request):
+	if 	request.method =='POST': 
+		pessoaBusca = request.POST.get ('pessoaBusca')
+		dataBuscaInicio = datetime.strptime(request.POST.get('dataBuscaInicio', ''), '%d/%m/%Y %H:%M:%S')
+		dataBuscaFim = datetime.strptime(request.POST.get('dataBuscaFim') , '%d/%m/%Y %H:%M:%S')
+
+		nome = Pessoa.objects.filter(id=pessoaBusca)
+		pessoas = Pessoa.objects.all().order_by('nome')
+
+		totalReceber = 0 
+		totalPagar = 0
+
+		
+# Create your views here.
